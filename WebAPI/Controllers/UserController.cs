71db9dbcce8c5a924243fb2a5050d3ea2af9d838
sale-b <12000.sale@gmail.com>
2021-04-01 14:36:25 +0200

    Bug fix

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -17,26 +17,23 @@ namespace WebAPI.Controllers
         private IMediator _mediatR;
         protected IMediator Mediator => _mediatR ??= HttpContext.RequestServices.GetService<IMediator>();
 
-
-
+        [HttpPost]
+        [Route("InsertClient")]
+        public async Task<IActionResult> InsertClient(InsertClientCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+        [HttpPost]
+        [Route("InsertEmp")]
+        public async Task<IActionResult> InsertEmp(InsertEmployeeCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await Mediator.Send(new GetUsersQuery()));
+        }
     }
-
-    [HttpPost]
-            [Route("InsertClient")]
-            public async Task<IActionResult> InsertClient(InsertClientCommand command)
-            {
-                return Ok(await Mediator.Send(command));
-            }
-            [HttpPost]
-            [Route("InsertEmp")]
-            public async Task<IActionResult> InsertEmp(InsertEmployeeCommand command)
-            {
-                return Ok(await Mediator.Send(command));
-            }
-            [HttpGet]
-            [Route("GetUsers")]
-            public async Task<IActionResult> GetUsers()
-            {
-                return Ok(await Mediator.Send(new GetUsersQuery()));
-            }
 }
